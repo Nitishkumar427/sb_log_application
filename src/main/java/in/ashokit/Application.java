ff162a7e7aca938950dd4bc7e765414e36208e83
Nitish <Nitishkumar42622@gmail.com>
2025-09-17 12:56:18 +0530

    age update variable

--- src/main/java/in/ashokit/Application.java
+++ src/main/java/in/ashokit/Application.java
@@ -12,6 +12,7 @@ public class Application {
 
 	public static void main(String[] args) {
 		int i=10;
+		int age=50;
 
 		ConfigurableApplicationContext ctxt =
 				SpringApplication.run(Application.class, args);
